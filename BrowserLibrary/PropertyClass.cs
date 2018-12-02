2d5d1f9144f4c324ddc0a6c73786b8c4ca81fb01
Zhecha <j.prejs@yandex.com>
2018-12-02 18:39:07 +0300

    add class of get information about property

---
+++ BrowserLibrary/PropertyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BrowserLibrary
+{
+    public class PropertyClass
+    {
+        public string _nameProperty { get; set; }
+        public string _typeProperty { get; set; }
+        public bool _public { get; set; }
+        public bool _write { get; set; }
+        public bool _read { get; set; }
+
+        public PropertyClass(PropertyInfo propertyInfo)
+        {
+            _nameProperty = propertyInfo.Name;
+            _typeProperty = propertyInfo.PropertyType.Name;
+            _public = true;
+            _write = propertyInfo.CanWrite;
+            _read = propertyInfo.CanRead;
+        }
+    }
+}
