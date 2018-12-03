3d3b555e171eddb187c6d70567515bb945b4934d
Zhecha <j.prejs@yandex.com>
2018-12-04 01:10:37 +0300

    add class with function of load assembly

---
+++ BrowserLibrary/InformAboutAssembly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BrowserLibrary
+{
+    public class InformAboutAssembly
+    {
+        public Collection<NameSpaceClass> _namespaces { get; set; }
+
+        public InformAboutAssembly(string path)
+        {
+            _namespaces = LoadAssembly(path);
+        }
+
+        private Collection<NameSpaceClass> LoadAssembly(string path)
+        {
+            Collection<NameSpaceClass> NameSpaces = new Collection<NameSpaceClass>();
+            Assembly assembly = Assembly.LoadFrom(path);
+            IEnumerator current = assembly.DefinedTypes.GetEnumerator();
+            while (current.MoveNext())
+            {
+                Type item = (Type)current.Current;
+                AddToCollection(item);
+            }
+            current.Reset();
+            return NameSpaces;
+        }
+    }
+}
