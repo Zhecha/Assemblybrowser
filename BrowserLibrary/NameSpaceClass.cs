252f45963c0151d04147229a73d1058304e2bbdb
Zhecha <j.prejs@yandex.com>
2018-12-04 00:42:04 +0300

    add class of get information about namespace

---
+++ BrowserLibrary/NameSpaceClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserLibrary
+{
+    public class NameSpaceClass
+    {
+        public string _nameNameSpace { get; set; }
+        public List<TypeData> _classes { get; set; }
+
+        public NameSpaceClass(string Namespace)
+        {
+            _nameNameSpace = Namespace;
+            _classes = new List<TypeData>();
+        }
+    }
+}
