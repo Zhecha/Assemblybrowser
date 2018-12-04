342b5ad21ae1cc6f339cade279a912edc69b9789
Zhecha <j.prejs@yandex.com>
2018-12-04 18:15:08 +0300

    add class for view model field

---
+++ assemblyBrowser/ViewField.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserLibrary;
+
+namespace assemblyBrowser
+{
+    public class ViewField
+    {
+        public string fieldClass;
+
+        public ViewField (FieldClass field)
+        {
+            string fieldstructure = "public";
+            if (field._static)
+                fieldstructure += " " + "static";
+            fieldstructure += field._typeField + " " + field._nameField;
+            fieldClass = fieldstructure;
+        }
+    }
+}
