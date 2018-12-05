375b2c663627c8d4059ea6dbae6ba23573846afe
Zhecha <j.prejs@yandex.com>
2018-12-05 17:31:56 +0300

    realize inotifypropertychanged interface

---
+++ assemblyBrowser/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace assemblyBrowser
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
