6bf8b78279ff2e3604419de98132190c4e0731b2
Zhecha <j.prejs@yandex.com>
2018-12-05 13:50:40 +0300

    add class view method

---
+++ assemblyBrowser/ViewMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserLibrary;
+
+namespace assemblyBrowser
+{
+    public class ViewMethod
+    {
+        public string _methodClass;
+
+        public ViewMethod(MethodClass method)
+        {
+            string methodacces = "";
+            if (method._public)
+                methodacces += "public";
+            if (method._static)
+                methodacces += " " + "static";
+            _methodClass = methodacces + " " + method._sign;
+        }
+    }
+}
