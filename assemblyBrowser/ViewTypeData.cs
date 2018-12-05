8580b108e48d828a7005230863c5f307af07e5de
Zhecha <j.prejs@yandex.com>
2018-12-05 14:11:42 +0300

    add class view typedata of initialize constructor

---
+++ assemblyBrowser/ViewTypeData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserLibrary;
+
+namespace assemblyBrowser
+{
+    public class ViewTypeData
+    {
+        public string _typedataClass;
+        public List<ViewField> viewFields;
+        public List<ViewMethod> viewMethods;
+        public List<ViewProperty> viewProperties;
+
+        public ViewTypeData(TypeData type)
+        {
+            string typedata = "";
+            if (type._public)
+                typedata += "public ";
+            if (type._abstract && !type._interface)
+                typedata += "abstract ";
+            if (type._interface)
+                typedata += "interface ";
+            if (type._class)
+                typedata += "class ";
+            _typedataClass = typedata + " " + type._nameClass;
+
+        }
+    }
+}
