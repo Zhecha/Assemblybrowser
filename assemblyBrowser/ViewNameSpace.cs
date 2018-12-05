2e6f758d128e699aad8ef93a9933a121cd0f0f96
Zhecha <j.prejs@yandex.com>
2018-12-05 16:09:03 +0300

    add view class namespace

---
+++ assemblyBrowser/ViewNameSpace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserLibrary;
+
+namespace assemblyBrowser
+{
+    public class ViewNameSpace
+    {
+        public string _namespaceClass;
+        public List<ViewTypeData> viewTypes;
+
+        public ViewNameSpace(NameSpaceClass nameSpace)
+        {
+            _namespaceClass = nameSpace._nameNameSpace;
+            viewTypes = new List<ViewTypeData>();
+            foreach (var vT in nameSpace._classes)
+            {
+                viewTypes.Add(new ViewTypeData(vT));
+            }
+        }
+    }
+}
