c3ef2a88eee7be51af0154803a0164e5bfedb405
Zhecha <j.prejs@yandex.com>
2018-12-06 23:11:33 +0300

    add class view for assembly

---
+++ assemblyBrowser/ViewAssembly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserLibrary;
+
+namespace assemblyBrowser
+{
+    public class ViewAssembly : ViewModel
+    {
+        private Command command;
+        List<NameSpaceClass> nameSpaceClasses;
+        public List<ViewNameSpace> views;
+
+        public ViewAssembly()
+        {
+            nameSpaceClasses = new List<NameSpaceClass>();
+            views = new List<ViewNameSpace>();
+            foreach(NameSpaceClass spaceClass in nameSpaceClasses)
+            {
+                views.Add(new ViewNameSpace(spaceClass));
+            }
+        }
+    }
+}
