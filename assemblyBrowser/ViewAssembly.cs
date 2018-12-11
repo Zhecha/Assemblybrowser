d3549b05a0db6c5094fad0ce4e0e46f746845c2b
Zhecha <j.prejs@yandex.com>
2018-12-11 18:43:36 +0300

    fix main view class

--- assemblyBrowser/ViewAssembly.cs
+++ assemblyBrowser/ViewAssembly.cs
@@ -4,23 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BrowserLibrary;
+using Microsoft.Win32;
 
 namespace assemblyBrowser
 {
     public class ViewAssembly : ViewModel
     {
-        private Command command;
         List<NameSpaceClass> nameSpaceClasses;
-        public List<ViewNameSpace> views;
+        public List<ViewNameSpace> Views {
+            get
+            {
+                List<ViewNameSpace> views1 = new List<ViewNameSpace>();
+                foreach (NameSpaceClass spaceClass in nameSpaceClasses)
+                {
+                    views1.Add(new ViewNameSpace(spaceClass));
+                }
+                return views1;
+            }
+        }
+
+        private Command command;
+        public Command Load
+        {
+            get => command = new Command(o => {
+                    OpenFileDialog open = new OpenFileDialog();
+                    if (open.ShowDialog() == false)
+                        return;
+                    string filename = open.FileName;
+                    InformAboutAssembly inform = new InformAboutAssembly(filename);
+                    nameSpaceClasses = inform._namespaces;
+                    OnPropertyChanged(nameof(Views));
+                });
+        }
 
         public ViewAssembly()
         {
             nameSpaceClasses = new List<NameSpaceClass>();
-            views = new List<ViewNameSpace>();
-            foreach(NameSpaceClass spaceClass in nameSpaceClasses)
-            {
-                views.Add(new ViewNameSpace(spaceClass));
-            }
         }
+
     }
 }
